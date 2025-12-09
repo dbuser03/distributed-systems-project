fed4213b2e6009833367af06178f7c7193e1686f
Michelangelo Bettini <125562933+BettiM7@users.noreply.github.com>
2025-12-09 10:31:54 +0100

    Revert "Feature/store data"

--- src/project_frontend/src/components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as StoryLink } from "./StoryLink";
-export { default as Layout } from "../layout/Layout";
-export { default as EmptyState } from "./EmptyState";
-export { default as NotFoundError } from "./NotFoundError";
-export { default as StoryHeader } from "../layout/story/StoryHeader";
-export { default as StoryContent } from "../layout/story/StoryContent";
-export { default as StoryVoteSection } from "../layout/story/StoryVoteSection";
-export * from "./filters";
-export * from "../icons";
--- src/project_frontend/src/lib/formatUtils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Formats a number for display (e.g., 1500 -> "1.5k")
- */
-export function formatScore(num: number): string {
-  if (num >= 1000) {
-    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "k";
-  }
-  return num.toString();
-}
