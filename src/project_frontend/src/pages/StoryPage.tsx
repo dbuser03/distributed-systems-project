93e2b0a07623c3be762887af907b3ea60b0765c2
dbuser03 <buserdaniele@gmail.com>
2025-12-10 11:19:57 +0100

    feat(app): Add file attachment functionality to stories and update UI
    Changelog: feature

--- src/project_frontend/src/pages/StoryPage.tsx
+++ src/project_frontend/src/pages/StoryPage.tsx
@@ -6,6 +6,7 @@ import {
   StoryContent,
   StoryVoteSection,
 } from "../components/ui";
+import { StoryAttachments } from "../components/ui/StoryAttachment";
 import { useStory } from "../hooks";
 
 function StoryPage() {
@@ -21,6 +22,11 @@ function StoryPage() {
       <article className="w-full max-w-5xl">
         <StoryHeader story={story} />
         <StoryContent content={story.content} />
+        {story.files && story.files.length > 0 && (
+          <div className="mt-8">
+            <StoryAttachments files={story.files} />
+          </div>
+        )}
         <StoryVoteSection
           storyId={story.id}
           upvotes={story.upvotes}
